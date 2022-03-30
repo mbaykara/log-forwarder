caaf3ef755ec6bbfbe613c626864e6078fa0416f
Mehmet Baykara <mehmet.baykara@teamviewer.com>
2022-03-30 17:31:33 +0200

    new funcs

--- go.mod
+++ go.mod
@@ -8,10 +8,12 @@ require (
 )
 
 require (
+	github.com/Azure/azure-pipeline-go v0.2.3 // indirect
 	github.com/Azure/azure-sdk-for-go v63.0.0+incompatible // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.22.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v0.13.2 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v0.9.1 // indirect
+	github.com/Azure/azure-storage-blob-go v0.14.0 // indirect
 	github.com/Azure/azure-storage-file-go v0.8.0 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
 	github.com/Azure/go-autorest/autorest v0.11.24 // indirect
@@ -23,8 +25,9 @@ require (
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.1+incompatible // indirect
 	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
-	github.com/google/uuid v1.1.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
+	github.com/mattn/go-ieproxy v0.0.1 // indirect
 	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/net v0.0.0-20220325170049-de3da57026de // indirect
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/streaming"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	gojsonq "github.com/thedevsaddam/gojsonq/v2"
 )
@@ -20,50 +23,40 @@ func jsonTOraw(log []byte){
 	res := gojsonq.New().JSONString(string(log)).Find("log")
 	fmt.Printf("rest %s",res)
 	data := []byte(fmt.Sprint(res))
-	var containername = "customcon"
-	accountName, accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME"), os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
-	if len(accountName) == 0 || len(accountKey) == 0 {
-		fmt.Print("Either the AZURE_STORAGE_ACCOUNT or AZURE_STORAGE_ACCESS_KEY environment variable is not set")
-	}
-		cred, err := azblob.NewSharedKeyCredential(accountName, accountKey)
-			if err != nil {
-		   fmt.Print("Invalid credentials: " + err.Error())
-	    }
+	var containername = "customcon3"
+	cred,accountName:= auth()
 
-	if err != nil {
-		fmt.Print("Invalid credentials with error: " + err.Error())
-	}
 	URL := fmt.Sprintf("https://%s.blob.core.windows.net/", accountName)
 	ctx := context.Background()
 
-	serviceClient, err := azblob.NewServiceClientWithSharedKey(URL, cred, nil)
-	if err != nil {
-		fmt.Print("Invalid credentials with while creating a servceClient error: " + err.Error())
-	}
 
-	fmt.Printf("Creating a container named %s\n", containername)
-	containerClient := serviceClient.NewContainerClient(containername)
-	_, err = containerClient.Create(ctx, nil)
-	if err != nil {
-		
-		fmt.Print("Error Code: %s",err)
-		
-		
-	}
+//disable if container exist////////////////////////////
+	// serviceClient, err := azblob.NewServiceClientWithSharedKey(URL, cred, nil)
+	// if err != nil {
+	// 	fmt.Print("Invalid credentials with while creating a servceClient error: " + err.Error())
+	// }
+
+	// fmt.Printf("Creating a container named %s\n", containername)
+	// containerClient := serviceClient.NewContainerClient(containername)
+	// _, err = containerClient.Create(ctx, nil)
+	// if err != nil {	fmt.Print("Error Code: %s",err)	}
+///////////////////////////////////////////////////////////		
 
 	fmt.Printf("\nUploading logs ...\n")
 	
-	blobName := "quickstartblobs"
+	blobName := "foo.txt"
 	blobClient, err := azblob.NewBlockBlobClientWithSharedKey(URL+containername+"/"+blobName, cred, nil)
 	if err != nil {
 		fmt.Print(err)
 	}
-fmt.Printf("Seccess ...\n")
+   fmt.Printf("Seccess ...\n")
 	// Upload to data to blob storage
 	_, err = blobClient.UploadBufferToBlockBlob(ctx, data, azblob.HighLevelUploadToBlockBlobOption{})
 	if err != nil {
 		fmt.Print("Failure to upload to blob: %+v", err)
 	}
+	fmt.Printf("Appending %s",containername)
+	appendBlob(containername,blobName, data, ctx)
 
 }
 
@@ -71,4 +64,36 @@ func main() {
 	
 		http.HandleFunc("/log", headers)
 		http.ListenAndServe(":8090", nil)
+}
+
+
+func appendBlob(c ,b string, d []byte,ctx context.Context){
+	cred, accountName := auth()
+	u := fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s", accountName,c,b)
+	appendBlobClient, err := azblob.NewAppendBlobClientWithSharedKey(u, cred, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+		_, err = appendBlobClient.Create(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	val := string(d)
+	fmt.Println(val)
+	_, err = appendBlobClient.AppendBlock(ctx, streaming.NopCloser(strings.NewReader(fmt.Sprintf("%s\n", val ))), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+}
+
+func auth()(c *azblob.SharedKeyCredential, s string) {
+	accountName, accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME"), os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
+	if len(accountName) == 0 || len(accountKey) == 0 {
+		fmt.Print("Either the AZURE_STORAGE_ACCOUNT_NAME or AZURE_STORAGE_ACCESS_KEY environment variable is not set")
+	}
+	cred, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cred,accountName
 }
